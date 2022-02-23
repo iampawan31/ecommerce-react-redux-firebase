32901eb6e8d8ff1e45f1a68bfebf186dd893db05
Pawan Kumar <surpawan@gmail.com>
2022-02-24 01:52:58 +0530

    Initial setup

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="absolute bottom-0 bg-black py-4 text-white left-0 right-0">
+      <div className="container mx-auto">Footer</div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
